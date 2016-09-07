5c80b3f763595aa68b1938ed1677ef7fdb353647
Angel <aleangrodriguez123@gmail.com>
2016-09-07 14:58:19 -0500

    Adding views and methods for register a contact

--- app/src/main/java/com/example/addressbook/RegisterContactActivity.java
+++ app/src/main/java/com/example/addressbook/RegisterContactActivity.java
@@ -2,12 +2,37 @@ package com.example.addressbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterContactActivity extends AppCompatActivity {
+
+    EditText edtName,edtLastname,
+            edtPhone,edtAddress,edtEmail;
+    Button btnRegisterContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_contact);
+
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtLastname = (EditText) findViewById(R.id.edtLastname);
+        edtPhone = (EditText) findViewById(R.id.edtPhone);
+        edtAddress = (EditText) findViewById(R.id.edtAddress);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+
+        btnRegisterContact = (Button) findViewById(R.id.btnRegisterContact);
+        btnRegisterContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerContact();
+            }
+        });
+    }
+
+    public void registerContact(){
+
     }
 }
