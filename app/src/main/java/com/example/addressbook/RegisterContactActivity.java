816410d253d7a1ad68a405d413d4053e1a892966
Angel <aleangrodriguez123@gmail.com>
2016-09-07 15:07:00 -0500

    Added register contact method

--- app/src/main/java/com/example/addressbook/RegisterContactActivity.java
+++ app/src/main/java/com/example/addressbook/RegisterContactActivity.java
@@ -1,16 +1,22 @@
 package com.example.addressbook;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.addressbook.model.Contact;
 
 public class RegisterContactActivity extends AppCompatActivity {
 
     EditText edtName,edtLastname,
             edtPhone,edtAddress,edtEmail;
     Button btnRegisterContact;
+
+    SQLiteDatabase db = openOrCreateDatabase("addressbook",MODE_PRIVATE,null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +40,22 @@ public class RegisterContactActivity extends AppCompatActivity {
 
     public void registerContact(){
 
+        String name = edtName.getText().toString();
+        String lastname = edtLastname.getText().toString();
+        String phone = edtPhone.getText().toString();
+        String address = edtAddress.getText().toString();
+        String email = edtEmail.getText().toString();
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Contacts" +
+                "(Name VARCHAR," +
+                "Lastname VARCHAR" +
+                "Phone VARCHAR" +
+                "Address VARCHAR" +
+                "Email VARCHAR);");
+
+        db.execSQL("INSERT INTO Contacts values " +
+                "('"+name+"','"+lastname+"','"+phone+"','"+address+"','"+email+"')");
+
+        Toast.makeText(RegisterContactActivity.this, "Se añadio el contacto", Toast.LENGTH_SHORT).show();
     }
 }
